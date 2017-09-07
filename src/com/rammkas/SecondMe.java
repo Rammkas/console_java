3081b679f906e76d08efb1974943be6a1ec3e9a3
Rammkas <rammi@list.ru>
2017-09-07 21:03:09 +0300

    add message about dev branch

--- src/com/rammkas/SecondMe.java
+++ src/com/rammkas/SecondMe.java
@@ -12,6 +12,7 @@ public class SecondMe {
     {
         System.out.println("From class in other file in project package.");
         System.out.println("Change is coming.");
+        System.out.println("We are in dev branch on githun in net.");
         System.out.printf("Inner variable is: %d%n", variable_1);
     }
 }
